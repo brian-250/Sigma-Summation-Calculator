ea827c5fda2660375235d499e700f07b9b2155bc
brian-250 <brianmilian@csu.fullerton.edu>
2023-11-07 13:56:32 -0800

    Add functions and buttons for summation calculator

--- Sigma Summation Calculator/ViewController.swift
+++ Sigma Summation Calculator/ViewController.swift
@@ -6,14 +6,78 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var lastValue: UITextField!
+    @IBOutlet weak var beginValue: UITextField!
+    @IBOutlet weak var expressionField: UITextField!
+    @IBOutlet weak var displaySolution: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lastValue.delegate = self
+        lastValue.placeholder = "Enter a number"
+        expressionField.delegate = self
+        expressionField.placeholder = "Enter an expression"
+        beginValue.delegate = self
+        beginValue.placeholder = "Enter a number"
+    }
+    @IBAction func clearTextFields(_ sender: Any) {
+    }
+    @IBAction func divideNumbers(_ sender: Any) {
+    }
+    @IBAction func multiplyNumbers(_ sender: Any) {
+    }
+    @IBAction func subtractNumbers(_ sender: Any) {
+    }
+    @IBAction func addNumbers(_ sender: Any) {
+    }
+    @IBAction func solveMathProblem(_ sender: Any) {
+        guard let firstText = beginValue.text, let lastText = lastValue.text, let expressionText = expressionField.text else { return }
+
+        guard let firstNum = Int(firstText), let lastNum = Int(lastText) else { return }
+
+        var total = 0
+
+        let sequence = stride(from: firstNum, to: (lastNum+1), by: 1)
+
+        for number in sequence {
+            let replacedExpression = expressionText.replacingOccurrences(of: "i", with: String(number))
+            let expressionToEvaluate = NSExpression(format: replacedExpression)
+            if let result = expressionToEvaluate.expressionValue(with: nil, context: nil) as? Double {
+                total += Int(result)
+            }
+        }
+        // Update the displaySolution label with the total
+        displaySolution.text = "Total: \(total)"
     }
 
 
+    @IBAction func addDecimalToNumbers(_ sender: Any) {
+    }
+    @IBAction func displayNumberNine(_ sender: Any) {
+    }
+    @IBAction func displayNumberEight(_ sender: Any) {
+    }
+    @IBAction func displayNumberSeven(_ sender: Any) {
+    }
+    @IBAction func displayNumberSix(_ sender: Any) {
+    }
+    @IBAction func displayNumberFive(_ sender: Any) {
+    }
+    @IBAction func displayNumberFour(_ sender: Any) {
+    }
+    @IBAction func displayNumberThree(_ sender: Any) {
+    }
+    @IBAction func displayNumberTwo(_ sender: Any) {
+    }
+    @IBAction func displayNumberOne(_ sender: Any) {
+    }
+    @IBAction func displayNumberZero(_ sender: Any) {
+    }
 }
 
