796db3bd40c3373c6992aaaf61ba99c64dcd8bf9
brian-250 <brianmilian@csu.fullerton.edu>
2023-11-07 09:39:27 -0800

    Initial Commit

---
+++ Sigma Summation Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sigma Summation Calculator
+//
+//  Created by csuftitan on 11/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
